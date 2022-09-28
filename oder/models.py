edc018c22b1da1aad1e02b4b73303997b8876dba
jamkia01010 <ahmedjamal100@gmail.com>
2022-09-28 20:25:01 +0200

    add the accounts and auth and proflie

--- oder/models.py
+++ oder/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from utils.code_grente import grente_code
+from django.utils import timezone
 # Create your models here.
+STATUS = (
+    ('receieved','receieved'),
+    ('processed','processed'),
+    ('shiped','shiped'),
+    ('delivered','delivered')
+)
+class Oders(models.Model):
+    code = models.CharField(max_length=8 , default= grente_code)
+    oder_time = models.DateTimeField(default=timezone.now)
+    deilevry_time = models.DateTimeField()
+    status = ''
+
+
+class OderDeitl(models.Model):
+    pass
--- project/settings.py
+++ project/settings.py
@@ -154,4 +154,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'tchactive@gmail.com'
-EMAIL_HOST_PASSWORD = 'iixxktyhnsvjwmrh'
+EMAIL_HOST_PASSWORD = ''
